43fec613a801e965662c7eceeae40e6d149dcd43
Wolf Thomsen <git@wolthom.com>
2021-12-02 21:51:43 +0100

    try using reqwest to get input data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv;
+use reqwest;
+use std::env;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    dotenv::dotenv().ok();
+    let token = env::var("AOC_SESSION_TOKEN")
+        .expect("Session token (AOC_SESSION_TOKEN) not found in environment variables");
+    let client = reqwest::blocking::Client::builder().build()?;
+    Ok(())
 }
