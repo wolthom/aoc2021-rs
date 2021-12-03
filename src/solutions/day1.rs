1f069d30bf8a8a7d89f8aa20b7e85f4b3823fefa
Wolf Thomsen <git@wolthom.com>
2021-12-03 20:47:43 +0100

    implement solution for day 1 part 1

--- src/solutions/day1.rs
+++ src/solutions/day1.rs
@@ -1,7 +1,31 @@
-pub fn part1(_inp: String) -> () {
-    println!{"Hello World!"};
+use std::fs::File;
+
+fn inp_to_vec(inp: &str) -> Vec<usize> {
+    inp
+    .split_whitespace()
+    .map(|el| el.parse::<usize>())
+    .collect::<Result<Vec<_>, _>>()
+    .expect("Input could not be parsed into integer")
 }
 
-pub fn part2(_inp: String) -> () {
+pub fn part1(inp: String) -> () {
+    // Turn string of lines into vector of integers
+    let vals = inp_to_vec(&inp);
+    
+    let res = vals.windows(2).fold(0, |acc, win| {
+        if win[1] > win[0] {
+            acc + 1
+        } else {
+            acc
+        }
+    });
+
+    println!("Result for day 1, part 1: {}", res);
+}
+
+pub fn part2(inp: String) -> () {
+    // Turn string of lines into vector of integers
+    let vals = inp_to_vec(&inp);
+
     println!{"Hello World!"};
 }
