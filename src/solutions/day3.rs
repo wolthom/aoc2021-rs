b22f2c461cccec736a720ace0ec218acae968c71
Wolf Thomsen <git@wolthom.com>
2021-12-04 15:32:19 +0100

    finish day 3 part 2 solution

--- src/solutions/day3.rs
+++ src/solutions/day3.rs
@@ -1,17 +1,20 @@
 const BIT_WIDTH: usize = 12;
 const BIT_MASK: usize = 0b1111_1111_1111;
+// const BIT_WIDTH: usize = 5;
+// const BIT_MASK: usize = 0b11111;
+
 
 // region: Part 1
 pub fn part1(inp: String) -> () {
     // Set up data
     let nums = inp.lines().map(|l| usize::from_str_radix(l, 2).unwrap()).collect::<Vec<_>>();
-    let mut counts = vec![0usize; BIT_WIDTH];
-    // Update bit counts for each number
-    nums.iter().for_each(|num| count_bits(&mut counts, *num));
+    let counts = count_bits(&nums);
+
     // Parse vec into gamma
     let gamma = counts.iter().enumerate().fold(0usize, |acc, (idx, num)| {
+        let shift = BIT_WIDTH - idx - 1;
         if *num > (nums.len() / 2) {
-            acc |  (1 << idx)
+            acc |  (1 << shift)
         } else {
             acc
         }
@@ -25,20 +28,52 @@ pub fn part1(inp: String) -> () {
 pub fn part2(inp: String) -> () {
     // Set up data
     let nums = inp.lines().map(|l| usize::from_str_radix(l, 2).unwrap()).collect::<Vec<_>>();
-    let mut counts = vec![0usize; BIT_WIDTH];
-    // Update bit counts for each number
-    nums.iter().for_each(|num| count_bits(&mut counts, *num));
-    dbg!(&counts);
+
+    let ox_val = select_val(&nums, false);
+
+    let co2_val = select_val(&nums, true);
+    println!("Result for day 3, part 1: {}", ox_val * co2_val);
+}
+
+fn predicate(num: usize, pos: usize, bit: usize) -> bool {
+    let shift = BIT_WIDTH - pos - 1;
+    ((num & 1 << shift) >> shift ) == bit
+}
+
+fn select_val(nums: &[usize], co2_pred: bool) -> usize {
+    let mut selection = nums.to_owned();
+    let mut idx = 0usize;
+    let mut counts = count_bits(&selection);
+    while selection.len() > 1 {
+        let n = selection.len() / 2 + selection.len() % 2;
+        let mut bit = if counts[idx] >= n {
+            1usize
+        } else {
+            0usize
+        };
+        if co2_pred {
+            bit = bit ^ 1;
+        }
+        selection = selection.into_iter().filter(|num| predicate(*num, idx, bit)).collect();
+        counts = count_bits(&selection);
+        idx += 1;
+    }
+    selection[0]
 }
 // endregion
 
 // region: Common functions
-fn count_bits(counts: &mut Vec<usize>, num: usize) {
-    for idx in 0..BIT_WIDTH {
-        let mask = 1usize << idx;
-        if (num & mask) > 0 {
-            counts[idx] += 1;
-        }
+fn count_bits(nums: &[usize]) -> Vec<usize> {
+    let mut out = vec![0; BIT_WIDTH];
+    for num in nums{
+        for idx in 0..BIT_WIDTH {
+            let shift = BIT_WIDTH - idx - 1;
+            let mask = 1usize << shift;
+            if (num & mask) > 0 {
+                out[idx] += 1;
+            }
+        }        
     }
+    out
 }
 // endregion 
