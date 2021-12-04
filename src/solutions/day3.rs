3017321ca80135dd53de15811eed271c39e8d156
Wolf Thomsen <git@wolthom.com>
2021-12-04 13:38:11 +0100

    improve day 3 part 1 solution via bit shifting

--- src/solutions/day3.rs
+++ src/solutions/day3.rs
@@ -1,38 +1,32 @@
+const BIT_WIDTH: usize = 12;
+const BIT_MASK: usize = 0b1111_1111_1111;
+
 // region: Part 1
 pub fn part1(inp: String) -> () {
-    let num_bits = inp.lines().peekable().peek().unwrap().len();
-    let num_lines = inp.lines().count();
-    let mut counts = vec![0usize; num_bits];
-    inp.lines().for_each(|line| count_bits(&mut counts, line));
-    let gamma = counts.iter().map(|&count| {
-        if count > (num_lines / 2) {
-            '1'
+    // Set up data
+    let nums = inp.lines().map(|l| usize::from_str_radix(l, 2).unwrap()).collect::<Vec<_>>();
+    let mut counts = vec![0usize; BIT_WIDTH];
+    // Update bit counts for each number
+    nums.iter().for_each(|num| count_bits(&mut counts, *num));
+    // Parse vec into gamma
+    let gamma = counts.iter().enumerate().fold(0usize, |acc, (idx, num)| {
+        if *num > (nums.len() / 2) {
+            acc |  (1 << idx)
         } else {
-            '0'
-        }
-    }).collect::<String>();
-    let epsilon = gamma.chars().map(|c| {
-        match c {
-            '0' => '1',
-            '1' => '0',
-            _ => panic!("Encountered unexpected character"),
-        }
-    }).collect::<String>();
-    let gamma =  usize::from_str_radix(&gamma, 2).unwrap();
-    let epsilon =  usize::from_str_radix(&epsilon, 2).unwrap();
-
-    let res = gamma * epsilon;
-    println!("Result for day 3, part 1: {}", res);
-}
-
-fn count_bits(counts: &mut Vec<usize>, line: &str) {
-    counts.iter_mut().zip(line.chars()).for_each(|(count, char)|{
-        match char {
-            '0' => (),
-            '1' => *count += 1,
-            _ => panic!("Encountered unexpected character"),
+            acc
         }
     });
+    let epsilon = gamma ^ BIT_MASK;
+    println!("Result for day 3, part 1: {}", epsilon * gamma);
+}
+
+fn count_bits(counts: &mut Vec<usize>, num: usize) {
+    for idx in 0..BIT_WIDTH {
+        let mask = 1usize << idx;
+        if (num & mask) > 0 {
+            counts[idx] += 1;
+        }
+    }
 }
 // endregion
 
